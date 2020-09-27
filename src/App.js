93ed0eca9cd5948ea80fd820c1ed91a93a9e8fd2
jsusilo <enr@bk.ru>
2020-09-27 20:19:39 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/font-awesome.min.css";
+import "./assets/css/default.css";
+import "./assets/css/btn-style.css";
+import "./assets/css/venobox/venobox.css";
+import "./assets/css/owl.carousel.css";
+import "./assets/css/header-menu.css";
+import "./assets/css/meanmenu.min.css";
+import "./assets/css/slick.css";
+import "./style.css";
+
+import "./assets/css/responsive.css";
+import "./assets/css/widget.css";
+
+import Home from "./pages/Home";
+// import About from "./pages/About";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return <Route exact path="/" component={About} />;
+  return <Route exact path="/" component={Home} />;
 }
 
 export default App;
