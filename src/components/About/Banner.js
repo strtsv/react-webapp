7083c6b480a18f4517bbfba633896e4871003ccd
jsusilo <enr@bk.ru>
2020-10-05 00:29:11 +0500

    about banner comp

---
+++ src/components/About/Banner.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Banner extends React.Component {
+  render() {
+    return (
+      <div className="witr_banner_area">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <div className="witr_single_banner">
+                <h2>About</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Banner;
