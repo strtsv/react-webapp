32d8d898b280a9c7537b02cde442cbbc22e3d876
jsusilo <enr@bk.ru>
2020-10-16 21:05:53 +0500

    Home service area comp

---
+++ src/components/Home/ServiceArea.js
@@ -0,0 +1,339 @@
+import React from "react";
+import bar from "../../assets/images/bar/bar.png";
+
+class ServiceArea extends React.Component {
+  render() {
+    return (
+      <div className="witr_service_area home_1">
+        <div className="container">
+          <div className="row">
+            {}
+            {}
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <div className="witr_section_title text-center">
+                <div className="witr_sec_title_inner">
+                  <h6>Service</h6>
+                  <img src={bar} alt="fward" />
+                  <h2>Awesome Website Templates</h2>
+                  <h2>for Your Business</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          {}
+          <ul className="nav witr_tabs_menu">
+            {}
+            <li className="witr_s_nav">
+              <a data-toggle="tab" href="#s1">
+                <div className="witr_single_service">
+                  <div className="witr_single_service_content">
+                    <i className="fa fa-gears  " />
+                    <h3>Business Strategy</h3>
+                  </div>
+                </div>
+              </a>
+            </li>
+            {}
+            <li className="witr_s_nav">
+              <a data-toggle="tab" className="active show" href="#s2">
+                <div className="witr_single_service">
+                  <div className="witr_single_service_content">
+                    <i className="fa fa-institution  " />
+                    <h3>Marketing Platfrom</h3>
+                  </div>
+                </div>
+              </a>
+            </li>
+            {}
+            <li className="witr_s_nav">
+              <a data-toggle="tab" href="#s3">
+                <div className="witr_single_service">
+                  <div className="witr_single_service_content">
+                    <i className="fa fa-bar-chart " />
+                    <h3>Portfolio & Resource</h3>
+                  </div>
+                </div>
+              </a>
+            </li>
+            {}
+            <li className="witr_s_nav">
+              <a data-toggle="tab" href="#s4">
+                <div className="witr_single_service">
+                  <div className="witr_single_service_content">
+                    <i className="fa fa-group " />
+                    <h3>Capital Markets</h3>
+                  </div>
+                </div>
+              </a>
+            </li>
+          </ul>
+          <div className="tab-content">
+            <div className=" active show in fade tab-pane" id="s1">
+              <div className="row">
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-gears  " />
+                    <h3>
+                      <a href="#">Application Development</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-bar-chart " />
+                    <h3>
+                      <a href="#">Beneficial Strategies</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-laptop" />
+                    <h3>
+                      <a href="#">Human Resource</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-child " />
+                    <h3>
+                      <a href="#">Digital Markectplace</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+              </div>
+              {}
+            </div>
+            <div className="fade tab-pane" id="s2">
+              <div className="row">
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-gears  " />
+                    <h3>
+                      <a href="#">Responsive design</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-bar-chart " />
+                    <h3>
+                      <a href="#">Log workout results</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-laptop" />
+                    <h3>
+                      <a href="#">Track your Progress</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-child " />
+                    <h3>
+                      <a href="#">Pixel-Perfect Design</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+              </div>
+              {}
+            </div>
+            <div className="fade tab-pane" id="s3">
+              <div className="row">
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-gears  " />
+                    <h3>
+                      <a href="#">Responsive design</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-bar-chart " />
+                    <h3>
+                      <a href="#">Log workout results</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-laptop" />
+                    <h3>
+                      <a href="#">Track your Progress</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-child " />
+                    <h3>
+                      <a href="#">Pixel-Perfect Design</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+              </div>
+              {}
+            </div>
+            <div className="fade tab-pane" id="s4">
+              <div className="row">
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-gears  " />
+                    <h3>
+                      <a href="#">Application Development</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-bar-chart " />
+                    <h3>
+                      <a href="#">Beneficial Strategies</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-laptop" />
+                    <h3>
+                      <a href="#">Human Resource</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+                {}
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <div className="witr_service_tab">
+                    <i className="fa fa-child " />
+                    <h3>
+                      <a href="#">Digital Markectplace</a>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing lit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua amet, conse.
+                    </p>
+                  </div>{" "}
+                  {}
+                </div>
+              </div>
+              {}
+            </div>
+          </div>
+          {}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ServiceArea;
