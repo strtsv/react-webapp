37b1233b54c8640654570b4e2afd774285924365
jsusilo <enr@bk.ru>
2020-10-20 21:53:40 +0500

    Home TestomonialArea comp

---
+++ src/components/Home/TestomonialArea.js
@@ -0,0 +1,142 @@
+import React from "react";
+import i1 from "../../assets/images/testomonial/t1.png";
+import i2 from "../../assets/images/testomonial/t2.png";
+import i3 from "../../assets/images/testomonial/t3.png";
+
+class TestomonialArea extends React.Component {
+  render() {
+    return (
+      <div className="witr_testomonial_area">
+        <div className="container">
+          <div className="row witr_testomonial_slide owl-carousel curosel-style">
+            {}
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <div className="witr_background_test">
+                <div className="witr_testi_main">
+                  <div className="witr_test_part ">
+                    <img src={i1} alt />
+                  </div>
+                  <div className="witr_ns_part ">
+                    <h1>Diana Austin</h1>
+                    <span>Seo & Founder</span>
+                  </div>
+                  <div className="witr_test_content">
+                    <p>
+                      We always act like everyon adipisicing elit, sed do
+                      eiusmod tempor ididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim , quis nostrud rcitati ullamco laboris
+                      nisi ut aliquip ex ea commodo at. Duis aute irure dolor
+                      innd erite always act like everyon
+                    </p>
+                  </div>
+                  <div className="witr_test_name">
+                    <h3>good design</h3>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {}
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <div className="witr_background_test">
+                <div className="witr_testi_main">
+                  <div className="witr_test_part ">
+                    <img src={i2} alt />
+                  </div>
+                  <div className="witr_ns_part ">
+                    <h1>Praveen Gupta</h1>
+                    <span>Development</span>
+                  </div>
+                  <div className="witr_test_content">
+                    <p>
+                      We always act like everyon adipisicing elit, sed do
+                      eiusmod tempor ididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim , quis nostrud rcitati ullamco laboris
+                      nisi ut aliquip ex ea commodo at. Duis aute irure dolor
+                      innd erite always act like everyon
+                    </p>
+                  </div>
+                  <div className="witr_test_name">
+                    <h3>good design</h3>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {}
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <div className="witr_background_test">
+                <div className="witr_testi_main">
+                  <div className="witr_test_part ">
+                    <img src={i3} alt />
+                  </div>
+                  <div className="witr_ns_part ">
+                    <h1>Raveen Gupta</h1>
+                    <span>co & Founder</span>
+                  </div>
+                  <div className="witr_test_content">
+                    <p>
+                      We always act like everyon adipisicing elit, sed do
+                      eiusmod tempor ididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim , quis nostrud rcitati ullamco laboris
+                      nisi ut aliquip ex ea commodo at. Duis aute irure dolor
+                      innd erite always act like everyon
+                    </p>
+                  </div>
+                  <div className="witr_test_name">
+                    <h3>good design</h3>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                    <span>
+                      <i className="fa fa-star" />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TestomonialArea;
